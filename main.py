b0b69b2be4d934849fe4e8a3598106580dec657b
CallMeMichal <michaltulej1@gmail.com>
2023-03-14 09:48:38 +0100

    1 chang

--- main.py
+++ main.py
@@ -25,9 +25,26 @@ print("Pytania kontrolne")
 p1=input("Jak masz na imie? :")
 p2=input("W jakich okolicznosciach czytasz ksiazki najczesciej? :")
 
-p3=print("Najczęstszym sposobem spędzania wolnego czasu jest dla Ciebie: ")
-a=input("a) oglądanie telewizji/filmów/seriali")
-b=input("b) czytanie książek/czasopism")
-c=input("c) słuchanie muzyki")
-d=input("d) spotkania z rodziną/przyjaciółmi")
+q1=print("1.Najczęstszym sposobem spędzania wolnego czasu jest dla Ciebie: ")
+a1=print("a) oglądanie telewizji/filmów/seriali")
+b1=print("b) czytanie książek/czasopism")
+c1=print("c) słuchanie muzyki")
+d1=print("d)  inne,jakie ?")
+aa1=input("Twoja odpowiedz :")
+
+
+q2=print("2.W jakich okolicznościach czytasz książki najczęściej?  ")
+a2=print("a) podczas podróży")
+b2=print("b) w czasie wolnym (po pracy, na urlopie)")
+c2=print("c) podczas pracy/nauki (to ich element)")
+d2=print("d) inne,jakie ?")
+aa2=input("Twoja odpowiedz :")
+
+
+q3=print("3.Jeżeli spędzasz czas wolny czytając książki, jaki jest główny powód takiego wyboru?  ")
+a3=print("a) chęć poszerzenia wiedzy")
+b3=print("b) czytanie mnie relaksuje/odpręża")
+c3=print("c) fakt, że czytanie jest modne")
+d3=print("d) inne,jakie ?")
+aa3=input("Twoja odpowiedz :")
 
