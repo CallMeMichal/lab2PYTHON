0a0eb8c5cd490094878927765dd9c2e22d2eeda0
CallMeMichal <michaltulej1@gmail.com>
2023-03-14 09:37:02 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+#zad2
+#l1 = float(input("Podaj liczbe 1: "))
+#l2 = float(input("Podaj liczbe 2: "))
+
+#znak=input("Podaj znak + - * / // % **: ")
+
+#if   znak == "+":
+#    print(l1 + l2)
+#elif znak == "-":
+#    print(l1-l2)
+#elif znak == "*":
+#    print(l1 * l2)
+#elif znak == "/":
+#    print(l1 / l2)
+#elif znak == "//":
+#    print(l1 // l2)
+#elif znak == "%":
+#    print(l1 % l2)
+#elif znak == "**":
+#    print(l1 ** l2)
+#else:
+#    print("Wprowadz odpowiedni znak")
+#zad3
+print("Pytania kontrolne")
+p1=input("Jak masz na imie? :")
+p2=input("W jakich okolicznosciach czytasz ksiazki najczesciej? :")
+
+p3=print("Najczęstszym sposobem spędzania wolnego czasu jest dla Ciebie: ")
+a=input("a) oglądanie telewizji/filmów/seriali")
+b=input("b) czytanie książek/czasopism")
+c=input("c) słuchanie muzyki")
+d=input("d) spotkania z rodziną/przyjaciółmi")
+
